186589c74037920c0b9f2e7a97d0e91669c950d8
jhalak27 <gupta.jhalak00@gmail.com>
2020-05-14 22:03:17 +0530

    Added case for different input set S/M

--- task.cpp
+++ task.cpp
@@ -5,7 +5,21 @@ int main(){
 	int N; //N -> number of test cases (int as value can't exceed 2000)
 	cin>>N;
 	for(int i = 0; i < N; i++){
-		//perform calculation for each iniput set here
+		char c;
+		cin>>c;
+		if(c=='S'){
+			// for fission
+			float l, w, r; //l->length, w->width r->radius of the virus to make fission
+			cin>>l>>w>>r;
+			//perform calculation for fission here
+		}
+		else{
+			// for fusion
+			float r1, r2, d; //r1->radius of the first virus, r2->radius of the second virus d->distance between virus
+			cin>>r1>>r2>>d;
+			//perform calculation for fusion set here
+		}
+		
 	}
 	return 0;
 }
