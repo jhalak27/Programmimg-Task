bfaef894c8f5fcde588ac66790cb1dd1f766eaee
Jhalak Gupta <gupta.jhalak00@gmail.com>
2020-05-14 21:44:22 +0530

    Inputting number of test case

--- task.cpp
+++ task.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main(){
+	int N; //N -> number of test cases (int as value can't exceed 2000)
+	cin>>N;
+	for(int i = 0; i < N; i++){
+		//perform calculation for each iniput set here
+	}
 	return 0;
 }
